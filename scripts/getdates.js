dcaf3d88f5771b4da7dec7e1192259810df98588
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-04-26 17:28:57 -0700

    wrote my wk1 html, designed the css, wrote a js code that shows the last time the code was modified after getting the current date

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+var currentYear = new Date().getFullYear();
+var lastModifiedDate = document.lastModified;
+document.getElementById("currentYear").innerText = currentYear;
+document.getElementById("lastModified").innerText = lastModifiedDate;
