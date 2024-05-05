099df31bc9703f617bc085e3d47d5971df016777
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-05-04 23:24:28 -0700

    mobile view edit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,7 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('nav ul'):
-
-    hamburger.addEventListener('click', function() {
-        nav.classList.toggle('show');
-        hamburger.classList.toggle('close');
-    });
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav ul');
+hamburger.addEventListener('click', function() {
+    nav.classList.toggle('show');
+    hamburger.classList.toggle('close');
 });
