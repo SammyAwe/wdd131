282ca6c8762f7a969e8b8fbbe386dff7af48d6be
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-05-04 23:55:58 -0700

    js for date added

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,8 @@ hamburger.addEventListener('click', function() {
     nav.classList.toggle('show');
     hamburger.classList.toggle('close');
 });
+
+var currentYear = new Date().getFullYear();
+var lastModifiedDate = document.lastModified;
+document.getElementById("currentYear").innerText = currentYear;
+document.getElementById("lastModified").innerText = lastModifiedDate;
